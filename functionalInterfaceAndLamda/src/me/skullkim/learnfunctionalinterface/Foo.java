8ee74f2afebb5d2944cb3edaed6d9b1102408f8e
skullKim <kyk990328@gmail.com>
2021-11-20 18:12:51 +0900

    추가: 람다 변수 캡쳐 예제

--- functionalInterfaceAndLamda/src/me/skullkim/learnfunctionalinterface/Foo.java
+++ functionalInterfaceAndLamda/src/me/skullkim/learnfunctionalinterface/Foo.java
@@ -3,6 +3,7 @@ package me.skullkim.learnfunctionalinterface;
 import java.sql.SQLOutput;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.IntConsumer;
 import java.util.function.UnaryOperator;
 
 /**
@@ -10,16 +11,39 @@ import java.util.function.UnaryOperator;
  * 함수형 인터페이스 실습을 위한 엔트리 포인트
  *
  * @author: yunki Kim
- * @version: 3.0
+ * @version: 4.0
  * @since: 2021-11-20
  */
 
 public class Foo {
 
-    public static void main(String[] args) {
-        UnaryOperator<Integer> plus10 = (number) -> number + 10;
-        UnaryOperator<Integer> multiply2 = (number) -> number * 2;
+    private void run() {
+        int baseNumber = 10;
 
-        System.out.println(plus10.andThen(multiply2).apply(2));
+        // local class
+        class LocalClass {
+            void printBaseNumber() {
+                int baseNumber = 11;
+                System.out.println(baseNumber); // 11
+            }
+        }
+
+        // anonymous class
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer baseNumber) {
+                System.out.println(baseNumber); // 인자로 전달한 baseNumber
+            }
+        };
+
+        // Variable 'baseNumber' is already defined in the scope
+        IntConsumer printInt = (baseNumber) -> System.out.println(baseNumber);
+
+        printInt.accept(10);
+    }
+
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        foo.run();
     }
 }
