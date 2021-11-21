16b6c0120a4ec973ac9baa803a3b680e16ac3650
skullKim <kyk990328@gmail.com>
2021-11-21 13:14:16 +0900

    추가: forEach예제

--- interfaceDefaultMethodStaticMethod/src/me/skullkim/learninterface/App.java
+++ interfaceDefaultMethodStaticMethod/src/me/skullkim/learninterface/App.java
@@ -1,23 +1,29 @@
 package me.skullkim.learninterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <h1>인터페이스의 default method, static method 실습 엔트리 포인트</h1>
  *
  * @author yunkiKim
- * @version 1.0
+ * @version 2.0
  * @since 2021-11-20
  */
 
 public class App {
 
     /**
-     * 실습에 필요한 DefaultFoo의 인스턴스를 만들고 결과를 출력한다
+     * java8 공식 API에서 추가된 default method 실습
      * @param args Unused
      */
     public static void main(String[] args) {
-       Foo foo = new DefaultFoo("yunki");
-       foo.printName();
-       foo.printNameUpperCase();
-       Foo.printAnyThing();
+        List<String> names = new ArrayList<>();
+        names.add("yunki");
+        names.add("skull");
+        names.add("keesun");
+        names.add("toby");
+
+        names.forEach(System.out::println);
     }
 }
