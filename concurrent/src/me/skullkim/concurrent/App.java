bbfa6cfe61782db627bc74bd99214378e793af4e
skullKim <kyk990328@gmail.com>
2021-11-23 20:21:06 +0900

    추가: thenCompose()예제

--- concurrent/src/me/skullkim/concurrent/App.java
+++ concurrent/src/me/skullkim/concurrent/App.java
@@ -21,14 +21,25 @@ public class App {
      * @param args Unused
      */
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
-            System.out.println("hello " + Thread.currentThread().getName());
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Hello " + Thread.currentThread().getName());
             return "Hello";
-        }, executorService).thenRun(() -> {
-            System.out.println(Thread.currentThread().getName());
         });
+
+        CompletableFuture<String> future = hello.thenCompose(App::getWorld);
         System.out.println(future.get());
+    }
+
+    /**
+     * themCompose()실습을 위한 메서드
+     * @param message 문자열을 받는다
+     * @return CompletableFuture<String> multi thread작업이 끝난 결과를 반환
+     */
+    private static CompletableFuture<String> getWorld(String message) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println(message + Thread.currentThread().getName());
+            return message + "World";
+        });
     }
 
 }
