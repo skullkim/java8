739180df00e3b63050a811254544120a5f96df32
skullKim <kyk990328@gmail.com>
2021-11-23 21:05:59 +0900

    추가: java doc export 연습

---
+++ functionalInterfaceAndLamda/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"me.skullkim.learnfunctionalinterface","l":"App"},{"p":"me.skullkim.learnfunctionalinterface","l":"Foo"},{"p":"me.skullkim.learnfunctionalinterface","l":"Greeting"},{"p":"me.skullkim.learnfunctionalinterface","l":"Plus10"},{"p":"me.skullkim.learnfunctionalinterface","l":"RunSomething"}]
