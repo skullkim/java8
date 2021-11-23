b2ef34e19268d119ecb8dcd1c3acb6ddaa9f9cf4
skullKim <kyk990328@gmail.com>
2021-11-23 20:06:51 +0900

    추가 CompletFuture 콜백 예제

--- concurrent/src/me/skullkim/concurrent/App.java
+++ concurrent/src/me/skullkim/concurrent/App.java
@@ -16,46 +16,19 @@ import java.util.concurrent.*;
 public class App {
 
     /**
-     * concurrent programming을 실제 실습하는 함수
+     * Completable Future 을 실제 실습하는 함수
      *
      * @param args Unused
      */
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(4);
-
-        Callable<String> hello = () -> {
-            Thread.sleep(2000L);
-            return "hello";
-        };
-
-       Callable<String> java = () -> {
-           Thread.sleep(3000L);
-           return "Java";
-       };
-
-       Callable<String> keesun = () -> {
-           Thread.sleep(1000L);
-           return "keesun";
-       };
-
-       //incokeAll을 사용해 Future의 리스트를 만든다.
-       //invokeAll은 입력한 Callable이 모두 종료될때 까지 기다린다.
-       //따라서 결과값이 한번에 나온다.
-       List<Future<String>> futures =  executorService.invokeAll(
-               Arrays.asList(hello, java, keesun)
-       );
-       futures.forEach((future) -> {
-           try {
-               System.out.println(future.get());
-           } catch (InterruptedException | ExecutionException e) {
-               e.printStackTrace();
-           }
-       });
-
-       //invokeAny는 입력한 Callable를 수행하되 하나만 완료되면 해당 값만 반환하고 종료한다.
-       String s = executorService.invokeAny(Arrays.asList(hello, java, keesun));
-       System.out.println(s);
-       executorService.shutdown();
+        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("hello " + Thread.currentThread().getName());
+            return "Hello";
+        }, executorService).thenRun(() -> {
+            System.out.println(Thread.currentThread().getName());
+        });
+        System.out.println(future.get());
     }
 
 }
