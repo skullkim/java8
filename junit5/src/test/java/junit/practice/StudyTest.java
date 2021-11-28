3dabecf21668a9547444a7d838cd0f7967d70747
skullKim <kyk990328@gmail.com>
2021-11-28 19:06:20 +0900

    추가: 조건에 따른 테스트 실행

--- junit5/src/test/java/junit/practice/StudyTest.java
+++ junit5/src/test/java/junit/practice/StudyTest.java
@@ -1,6 +1,7 @@
 package junit.practice;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 import java.time.Duration;
 
@@ -13,26 +14,22 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 	@Test
 	@DisplayName("새로운 스터디 만들기")
+	@EnabledOnOs(OS.MAC)
+	@EnabledOnJre(JRE.JAVA_8)
+	@EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
 	public void createNewStudy() {
-		assertTimeout(Duration.ofMillis(100), () -> {
-			new Study(10);
-		});
-		assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-			new Study(10);
-		});
-		Study study = new Study(1);
-		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-		assertEquals("limit은 0보다 커야한다", exception.getMessage());
-		assertAll(
-			() -> assertNotNull(study),
-			() -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
-				() -> "스터디를 만들면" + StudyStatus.DRAFT + "상태다."),
-			() -> assertTrue(study.getLimit() > 0, "스터디 최대 참석가능 인원은 0보다 커야한다")
-		);
+		Study actual = new Study(100);
+		assertTrue(actual.getLimit() > 0);
 	}
 
 	@Test
